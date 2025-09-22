0941c98b9de5ed3c96996edd055609bfe1ea7aad
Krishna Kumar S R <simmadhari.ramadass.krishna.kumar@intel.com>
2025-09-22 18:06:40 +0530

    Yocto: Agilex7 FTile 1588PTP: Initial branch creation
    Scripts update
    
    Signed-off-by: Krishna Kumar S R <krishna.kumar.simmadhari.ramadass@altera.com>

--- agi027fc-si-devkit/src/sw/yocto/meta-agilex7-sed/recipes-devtools/scripts/files/2PortMCQ.sh
+++ agi027fc-si-devkit/src/sw/yocto/meta-agilex7-sed/recipes-devtools/scripts/files/2PortMCQ.sh
@@ -5,14 +5,27 @@ echo "1" > /proc/irq/31/smp_affinity && echo "1" > /proc/irq/32/smp_affinity && 
 
 echo -e "Clearing old PTPBridge rules Port - 0..."
 ptpbridge --port 0 --flush-all-keys
-echo -e "Clearing old TC rules Port - 0..."
-tc filter del dev eth1 egress
-tc qdisc del dev eth1 clsact
+
+FILTERS=$(tc filter show dev eth1 egress 2>/dev/null)
+if [[ -z "$FILTERS" ]]; then
+	 echo -e "No Filters attached to eth1. Continuing..."
+else
+	 echo -e "Clearing old TC rules Port - 0..."
+	 tc filter del dev eth1 egress
+	 tc qdisc del dev eth1 clsact
+fi
+
 echo -e "Clearing old PTPBridge rules Port - 1..."
 ptpbridge --port 1 --flush-all-keys
-echo -e "Clearing old TC rules Port - 1..."
-tc filter del dev eth2 egress
-tc qdisc del dev eth2 clsact
+
+FILTERS=$(tc filter show dev eth2 egress 2>/dev/null)
+if [[ -z "$FILTERS" ]]; then
+	 echo -e "No Filters attached to eth2. Continuing..."
+else
+	 echo -e "Clearing old TC rules Port - 1..."
+	 tc filter del dev eth2 egress
+	 tc qdisc del dev eth2 clsact
+fi
 
 echo -e "Flushing old IPv4 and IPv6 addresses and routes"
 ip addres flush eth1 && ip route flush dev eth1
